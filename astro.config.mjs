f59f0554bcf8d2b2ba78748c1b7f00ca76abe56f
RobertAKARobin <contact@robertakarobin.com>
2022-09-27 13:09:20 -0500

    example

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+import mdx from '@astrojs/mdx';
+
+export default defineConfig({
+	integrations: [
+		mdx(),
+	],
+});
+
---
+++ src/js/throwerror.ts
@@ -0,0 +1,5 @@
+export function throwError() {
+	console.log('About to throw an error...');
+
+	throw new Error('Oh no');
+}
